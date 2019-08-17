e9112736b1d0020e79478881fc855bd7001a5393
pkellner <peter@peterkellner.net>
2019-08-17 08:40:38 -0700

    tag helper not showing purple for primitive like "p"

---
+++ TagHelpersNotPurple/PTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpersNotPurple
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("p")]
+    public class PTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.AppendHtml("...p tag helper...");
+
+        }
+    }
+}
---
+++ TagHelpersNotPurple/PeterTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpersNotPurple
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("peter")]
+    public class PeterTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.AppendHtml("...peter tag helper...");
+        }
+    }
+}
